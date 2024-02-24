218421a98c5d79571df61ce36069926ae28aaa47
Ashley Frank <ashley.frank@ufl.edu>
2024-02-23 19:58:45 -0500

    exported actions, added state

--- src/context/book/state.js
+++ src/context/book/state.js
@@ -1,8 +1,13 @@
 // TODO: Load/parse 'favoriteBooks' from localStorage
+const favoriteBooks = localStorage.getItem('favoriteBooks')
+
 // TODO: Export initial state object as default
 // initial state should have keys bookSearchResults and favoriteBooks
 // bookSearchResults should be an empty array
 // favoriteBooks should be the value from localStorage or an empty array if localStorage value is falsy
-const initialState = {}
+const initialState = {
+    bookSearchResults: [],
+    favoriteBooks: favoriteBooks ? favoriteBooks : []
+}
 
 export default initialState
